a8decacf048af370d3ff8869756c73eaf0094a60
Beau Lyddon <lyddonb@gmail.com>
2018-11-13 08:58:28 -0600

    Add streaming dataflow job

---
+++ deploy_st.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python high_values.py --project rk-playground --topic projects/rk-playground/topics/topstdem1 --dataset gcpdemo --runner DataflowRunner --temp_location gs://tempgcpbucket1/user_value/temp
+
+
+#python high_values.py --project rk-playground --topic projects/rk-playground/topics/topstdem1 --dataset gcpdemo 
